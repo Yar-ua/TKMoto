43f07b0f698b71a9844e566adf85a7db44ebb06c
Yar <yar.ua.1985@gmail.com>
2016-09-21 19:18:51 +0300

    add form_fuel

--- app/controllers/fuel_stata_controller.rb
+++ app/controllers/fuel_stata_controller.rb
@@ -1,7 +1,15 @@
 class FuelStataController < ApplicationController
+#  before_action :set_bike #, only: [ :new, :show, :edit, :update, :destroy ]
   
   def new
-   
+    @fuel_stata = FuelStatum.new
   end
+
+
+#  private
+
+#  def set_bike
+#    @bike = Bike.find(:bike_id)
+#  end
 
 end
