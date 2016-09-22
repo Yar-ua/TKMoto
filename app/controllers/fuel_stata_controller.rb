5a8d2f9898247e3fe8e8ed98b9c4f5546a837fc0
Yar-ua <Yar.ua.1985@gmail.com>
2016-09-22 21:05:58 +0300

    worker method POST and no save to Fuel:DB

--- app/controllers/fuel_stata_controller.rb
+++ app/controllers/fuel_stata_controller.rb
@@ -1,28 +1,35 @@
 class FuelStataController < ApplicationController
-#  before_action :set_bike, only: [ :new, :show, :edit, :update, :destroy ]
+  #before_action :set_bike, only: [ :new, :create, :show, :edit, :update, :destroy ]
   
   def new
-    @fuel_stata = FuelStatum.new
+    @fuel_stata = FuelStata.new
   end
 
 
   def create
-#    @fuel_stata = FuelStatum.new(fuel_params)
- #   @fuel_stata.save
 
+    #@bike = Bike.find(params[:id])
+    #@bike.fuel_stata.create(fuel_params)
+    @fuel_stata = FuelStata.new(fuel_params)
+    #@fuel_stata.bike_id = params[:bike_id]
+    if @fuel_stata.save
+      render 'create'
+    else
+      render 'new'
     end
+
+
+  end
+
 end
 
 
-#  private
+  private
 
-#  def set_bike
-#    @bike = Bike.find(params[:id])
-#  end
-
-#  def fuel_params
-#    params.require(:fuelstatum).permit(:bike_id, :odo, :odo_delta, :refueling, :price_fuel, :fuel_station, :fuel_type)
-#  end
-
-
+  #def set_bike
+  #@bike = Bike.find([:bike_id])
   #end
+
+  def fuel_params
+    params.require(:fuel_stata).permit(:odo, :odo_delta, :refueling, :price_fuel, :fuel_station, :fuel_type)
+  end
