d653f4ce42e0a9eba01175023651c78632ab3f91
Yar-ua <Yar.ua.1985@gmail.com>
2016-09-26 22:50:26 +0300

    update Bike validation

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,8 +1,10 @@
 class Bike < ApplicationRecord
 	has_many :stata_fuels, dependent: :destroy
 
-	validates 	:name, presence: true, uniqueness: true, length: { maximum: 50 }
-	validates	:user_name, presence: true, length: { maximum: 30 }
-	validates	:year, presence: true
-	validates	:color, length: { maximum: 20 }
+	validates 	:name, presence: { message: "введите модель байка" }
+	validates	:name, length: { maximum: 30, message: "название модели - максимум 30 символов " }
+	validates	:user_name, presence: { message: "введите логин владельца" }
+	validates	:user_name, length: { maximum: 30, message: "логин владельца - максимум 30 символов" }
+	validates	:year, presence: { message: "введите год выпуска байка" }
+	validates	:color, length: { maximum: 20, message: "цвет байка - максимум 30 символов " }
 end
