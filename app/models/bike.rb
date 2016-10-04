4066b6afec9aeed00e16d43449696d3625510526
Yar <yar.ua.1985@gmail.com>
2016-10-04 18:36:17 +0300

    update Bikes: new/create form builder

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,10 +1,10 @@
 class Bike < ApplicationRecord
 	has_many :stata_fuels, dependent: :destroy
 
-	validates 	:name, presence: { message: "введите модель байка" }
-	validates	:name, length: { maximum: 30, message: "название модели - максимум 30 символов " }
-	validates	:user_name, presence: { message: "введите логин владельца" }
-	validates	:user_name, length: { maximum: 30, message: "логин владельца - максимум 30 символов" }
-	validates	:year, presence: { message: "введите год выпуска байка" }
-	validates	:color, length: { maximum: 20, message: "цвет байка - максимум 30 символов " }
+#	validates 	:name, presence: { message: "введите модель байка" }
+#	validates	:name, length: { maximum: 30, message: "название модели - максимум 30 символов " }
+#	validates	:user_name, presence: { message: "введите логин владельца" }
+#	validates	:user_name, length: { maximum: 30, message: "логин владельца - максимум 30 символов" }
+#	validates	:year, presence: { message: "введите год выпуска байка" }
+#	validates	:color, length: { maximum: 20, message: "цвет байка - максимум 30 символов " }
 end
