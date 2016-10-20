caa635e85634e0e59d98fbbdb3a05f4be33762f3
Yar <yar.ua.1985@gmail.com>
2016-10-20 23:42:10 +0300

    update About page

--- app/helpers/stata_fuels_helper.rb
+++ app/helpers/stata_fuels_helper.rb
@@ -1,19 +1,11 @@
 module StataFuelsHelper
 
-
   def get_stata_l_na_100_km
-
-     @stata_graph = []
-
+    @stata_graph = []
     @view_stata_fuel.each do |stata|
-      
       @stata_graph << (stata.refueling / (stata.odo_delta / 100)).round(2).to_f
-		
     end
-
   @json_stata_l_na_100_km = @stata_graph.to_json
-
   end
-
 
 end
