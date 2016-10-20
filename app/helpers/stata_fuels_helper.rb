e442f88c706bd36c79c9aefa19ad97816bb2c9b4
Yar <yar.ua.1985@gmail.com>
2016-10-20 20:59:14 +0300

    refactoring

--- app/helpers/stata_fuels_helper.rb
+++ app/helpers/stata_fuels_helper.rb
@@ -1,34 +1,19 @@
 module StataFuelsHelper
 
 
-  class StataGraph < StataFuel
-    attr_accessor :id
-    attr_accessor :l_na_100_km
-  end
-
-
-  def stata_l_na_100_km
+  def get_stata_l_na_100_km
 
      @stata_graph = []
-     a = []
-     b = []
-
-    id = 1
 
     @view_stata_fuel.each do |stata|
       
-      a = (stata.refueling / (stata.odo_delta / 100)).round(2).to_f
-	  b = id
-	  @stata_graph << a
-	  
-    id = id + 1
-	
+      @stata_graph << (stata.refueling / (stata.odo_delta / 100)).round(2).to_f
+		
     end
 
-  @json_stata = @stata_graph.to_json
+  @json_stata_l_na_100_km = @stata_graph.to_json
 
   end
-
 
 
 end
--- app/models/graph_fuel.rb
+++
@@ -1,2 +0,0 @@
-class GraphFuel < ApplicationRecord
-end
