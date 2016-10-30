fa0730d88f7076697d12fbe46c03fa4d68a9bab7
Yar <yar.ua.1985@gmail.com>
2016-10-30 11:08:32 +0200

    add login/logout by username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def after_sign_in_path_for(resource)
     bikes_path
@@ -9,5 +11,17 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resources_of_scope)
   	request.referrer
   end
+
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:email, :username)}
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :username, :password, :password_confirmation)}
+  end
+
+
+
+
 
 end
