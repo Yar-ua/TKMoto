66e4310753bda8411c6e70c58cf31bfd22f1a61d
Yar <yar.ua.1985@gmail.com>
2016-10-30 20:32:14 +0200

    Static pages tested

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
   describe "Start page" do
-    
     it "should have the content 'myCarousel'" do
       visit '/'
       expect(page).to have_content('Главная')
-    
     end
   end
 
+  describe "Start page" do
+    it "should have the content 'myCarousel'" do
+      visit '/home/start'
+      expect(page).to have_content('Главная')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'О проекте'" do
+      visit '/home/about'
+      expect(page).to have_content('О проекте')
+    end
+  end
+
+  
 end
