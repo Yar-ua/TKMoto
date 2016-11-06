2a0d53e9b7c0f39bea09455c0cc5f278e1464d08
Yar <yar.ua.1985@gmail.com>
2016-11-06 12:18:08 +0200

    add session_helper and User model methods for remember_token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  	  #sign yje user and redirect to yje user's show page
+  	  sign_in user
+  	  resirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination' # not quite right!
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+
+  def sign_in(user)
+  	remember_token = User.new_remember_token
+  	cookies.permanent[:remember_token] = remember_token #установка истечения remember_token 
+														# через 20 лет
+# аналогично cookies[:remember_token] = { value: remember_token, expires: 20.years.from_now.utc }
+  	user.update_attribute(:remember_token, User.encrypt(remember_token))
+  	self.current_user = user
+  end
+
+  def current_user=(user)   # аналогично self.current_user = ...
+  	@current_user = user
+  end
+
 end
