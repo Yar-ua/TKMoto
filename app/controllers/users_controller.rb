5421dd63dcc06e0e11c256cfede99dc305bbda10
Yar <yar.ua.1985@gmail.com>
2016-11-12 23:48:57 +0200

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   
 
   def show
-  	@user = User.find_by(params[:id])
+  	@user = User.find(params[:id])
   end
 
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,46 +4,31 @@ describe "User pages" do
 
   subject { page }
 
-#   describe 'index' do                   старые тесты без кучи юзеров для пагинации
-#    before do
-#      sign_in FactoryGirl.create(:user)
-#      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
-#      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
-#      visit users_path
-#    end
 
     describe 'index' do
       let(:user) { FactoryGirl.create(:user) }
       before(:each) do
         sign_in user
-        visit user_path
+        visit users_path
+      end
+    
+
+      it { should have_content 'All users' }
+
+
+      describe "pagination" do
+
+        before(:all) { 30.times { FactoryGirl.create(:user) } }
+        after(:all)  { User.delete_all }
+
+        it { should have_selector('div.pagination') }
       end
     end
-
-
-    it { should have_content 'All users' }
-
-
-    describe "pagination" do
-
-      before(:all) { 30.times { FactoryGirl.create(:user) } }
-      after(:all)  { User.delete_all }
-
-      it { should have_selector('div.pagination') }
-
-      it "should list each user" do
-        User.paginate(page: 1).each do |user|
-          expect(page).to have_selector('li', text: user.name)
-        end
-      end
-    end
-
-
 
 
     it 'should list each user' do
       User.all.each do |user|
-        expect(page).to have_selector('li', text: user.name)
+        expect(page).to have_selector("li", text: user.name)
       end
     end
   
