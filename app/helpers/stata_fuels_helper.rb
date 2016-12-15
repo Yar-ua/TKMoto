9bd84951db21bc4dc49a4c5eee9c4c54df2347e2
yar <yar.ua.1985@gmail.com>
2016-12-16 00:25:59 +0200

    add new refueling chart

--- app/helpers/stata_fuels_helper.rb
+++ app/helpers/stata_fuels_helper.rb
@@ -1,11 +1,21 @@
 module StataFuelsHelper
 
+
   def get_stata_l_na_100_km
     @stata_graph = []
     @view_stata_fuel.each do |stata|
       @stata_graph << (stata.refueling / (stata.odo_delta / 100)).round(2).to_f
     end
-  @json_stata_l_na_100_km = @stata_graph.to_json
+    @json_stata_l_na_100_km = @stata_graph.to_json
+  end
+
+
+  def get_stata_refueling_to_chart
+  	@stata_refueling = []
+  	@view_stata_fuel.each do |stata|
+      @stata_refueling << stata.refueling.to_f
+  	end
+    @json_stata_refueling = @stata_refueling.to_json
   end
 
 end
