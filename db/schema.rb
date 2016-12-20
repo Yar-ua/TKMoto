84bcae86c14c4150a5f77e41767f2e5eabae53a2
yar <yar.ua.1985@gmail.com>
2016-12-20 23:42:23 +0200

    refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112215146) do
+ActiveRecord::Schema.define(version: 20161220211426) do
 
   create_table "bikes", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 20161112215146) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["bike_id"], name: "index_stata_fuels_on_bike_id"
+  end
+
+  create_table "stata_repairs", force: :cascade do |t|
+    t.string   "type"
+    t.string   "note"
+    t.decimal  "price"
+    t.integer  "bike_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_stata_repairs_on_bike_id"
   end
 
   create_table "users", force: :cascade do |t|
